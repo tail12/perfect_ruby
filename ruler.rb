1afdd0f49e3b5cc27c4f6d361c33aed6b6501313
tail12 <tdallb12@gmail.com>
2017-11-03 05:24:33 +0900

    delete initialize method and add class method.

--- ruler.rb
+++ ruler.rb
@@ -1,12 +1,9 @@
 class Ruler
   attr_accessor :length
 
-  def initialize(length)
-    # いわゆるコンストラクタ
-    @length = length
+  def self.pair
+    [Ruler.new, Ruler.new]
   end
 end
 
-
-ruler = Ruler.new(30)
-puts ruler.length
+puts Ruler.pair
