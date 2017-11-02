cd7409b7338b070917c31fdf0ab147f43d4c04c0
tail12 <tdallb12@gmail.com>
2017-11-03 05:10:09 +0900

    refactoring.

--- ruler.rb
+++ ruler.rb
@@ -1,19 +1,12 @@
 class Ruler
   attr_accessor :length
 
-  def set_default_length
-    # length = 30ではメソッドは呼び出されず，lengthという名前のローカル変数が定義される
-    self.length = 30
-  end
-
-  def display_length
-    # Ruler#lengthの戻り値を出力している
-    puts length
+  def initialize(length)
+    # いわゆるコンストラクタ
+    @length = length
   end
 end
 
 
-ruler = Ruler.new
-ruler.set_default_length
-
-ruler.display_length
+ruler = Ruler.new(30)
+puts ruler.length
