bfdbe98a2aac27cb54ec224b7c8950ef6997851d
tail12 <tdallb12@gmail.com>
2017-11-03 05:02:08 +0900

    add set_default_length method.

--- ruler.rb
+++ ruler.rb
@@ -1,6 +1,11 @@
 class Ruler
   attr_accessor :length
 
+  def set_default_length
+    # length = 30ではメソッドは呼び出されず，lengthという名前のローカル変数が定義される
+    self.length = 30
+  end
+
   def display_length
     # Ruler#lengthの戻り値を出力している
     puts length
@@ -9,6 +14,6 @@ end
 
 
 ruler = Ruler.new
-ruler.length  = 30
+ruler.set_default_length
 
 ruler.display_length
